ad07a7f110aaa92a45c67587ebccc028665d12cc
Carlos Chitty <chitty.carlosj@gmail.com>
2022-05-02 17:35:19 -0400

    Update navigation to be compatible with react-router-v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import CountryPage from "./components/CountryPage";
 import HomePage from "./components/HomePage";
@@ -7,14 +7,10 @@ import HomePage from "./components/HomePage";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/country/:iso3">
-          <CountryPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/country/:iso3" element={<CountryPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
